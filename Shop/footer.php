a31e51f17bee086f2c602f426cfe74b515130dab
Horl Rayou <78066942+HanRaYu@users.noreply.github.com>
2022-09-05 08:34:59 +0700

    Add files via upload

---
+++ Shop/footer.php
@@ -0,0 +1,38 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php">home</a>
+         <a href="about.php">about</a>
+         <a href="shop.php">shop</a>
+         <a href="contact.php">contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="login.php">login</a>
+         <a href="register.php">register</a>
+         <a href="cart.php">cart</a>
+         <a href="orders.php">orders</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +855 97 74 24 842 Metfone</p>
+         <p> <i class="fas fa-phone"></i> +855 96 82 04 330 Smart </p>
+         <p> <i class="fas fa-envelope"></i> G-kut3434@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> banteaymeanchey, Cambodia </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+</div>
+</section>
